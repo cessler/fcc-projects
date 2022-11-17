80d119aedf1ff935c76abe8d84104a29bc366815
cessler <cetm@mailbox.org>
2022-11-17 20:40:26 +0100

    Initial structure for project "Visualize Data with a Choropleth Map"

---
+++ Data-Visualization/Visualize-Data-with-a-Choropleth-Map/script.js
@@ -0,0 +1,6 @@
+const usEducationDataUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
+const usCountyDataUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
+const width = 1200;
+const height = 600;
+const margins = { top: 20, left: 80, bottom: 100, right: 75 };
+
